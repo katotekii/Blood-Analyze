5b0874bd79620555eeac3d3be0af9fc4ff5b9f71
Katoteki <58405477+katotekii@users.noreply.github.com>
2022-02-11 18:44:06 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -18,72 +18,66 @@ local function thread()
     end)
 end
 
-AddEventHandler("open:evidence", function() -- event for item
-    if not opened then
-        local id = exports.ox_inventory:Search(1, GlobalState.AnalizedBlood)
-        for _, v in pairs(id) do
-            SendNUIMessage({
-                type = "showReport",
-                evidence = json.encode(v.metadata)
-            })
-        end
-        TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_CLIPBOARD", 0, true)  
-        ESX.ShowNotification("BACKSPACE to close")
-        thread()
-        opened = true
-    end
-end)
-
-
 if Config.UseExport then 
-    exports('analizeblood',function (data,slot)
+    exports('open:evidence',function (data,slot)
         exports.ox_inventory:useItem(data, function(data)
             if data then
-                exports.rprogress:Custom(
-                    {
-                        Async = true,
-                        Duration = 1000,
-                        Label = "ANALIZING BLOOD...",
-                        Easing = "easeLinear",
-                        DisableControls = {
-                            Mouse = false,
-                            Player = true,
-                            Vehicle = false
-                        }
-                    },
-                    function(e)
-                end) 
-                Wait(1000)
-                local id = exports.ox_inventory:Search(1, GlobalState.blood)
-                for _, v in pairs(id) do
-                    TriggerServerEvent("giveanalizedblood", v.metadata)
+                if not opened then
+                    local id = exports.ox_inventory:Search(1, GlobalState.AnalizedBlood)
+                    for _, v in pairs(id) do
+                        SendNUIMessage({
+                            type = "showReport",
+                            evidence = json.encode(v.metadata)
+                        })
+                    end
+                    TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_CLIPBOARD", 0, true)  
+                    ESX.ShowNotification("BACKSPACE to close")
+                    thread()
+                    opened = true
                 end
             end
         end)
     end)
 else
-    AddEventHandler("analizeblood", function()
-        exports.rprogress:Custom(
-            {
-                Async = true,
-                Duration = 1000,
-                Label = "ANALIZING BLOOD...",
-                Easing = "easeLinear",
-                DisableControls = {
-                    Mouse = false,
-                    Player = true,
-                    Vehicle = false
-                }
-            },
-            function(e)
-        end) 
-        Wait(1000)
-        local id = exports.ox_inventory:Search(1, GlobalState.blood)
-        for _, v in pairs(id) do
-            TriggerServerEvent("giveanalizedblood", v.metadata)
+    AddEventHandler("open:evidence", function() -- event for item
+        if not opened then
+            local id = exports.ox_inventory:Search(1, GlobalState.AnalizedBlood)
+            for _, v in pairs(id) do
+                SendNUIMessage({
+                    type = "showReport",
+                    evidence = json.encode(v.metadata)
+                })
+            end
+            TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_CLIPBOARD", 0, true)  
+            ESX.ShowNotification("BACKSPACE to close")
+            thread()
+            opened = true
         end
     end)
 end
+
+
+AddEventHandler("analizeblood", function()
+    exports.rprogress:Custom(
+        {
+            Async = true,
+            Duration = 1000,
+            Label = "ANALIZING BLOOD...",
+            Easing = "easeLinear",
+            DisableControls = {
+                Mouse = false,
+                Player = true,
+                Vehicle = false
+            }
+        },
+        function(e)
+    end) 
+    Wait(1000)
+    local id = exports.ox_inventory:Search(1, GlobalState.blood)
+    for _, v in pairs(id) do
+        TriggerServerEvent("giveanalizedblood", v.metadata)
+    end
+end)
 
 exports["qtarget"]:AddCircleZone("analizeblood", vector3(367.08, -1592.64, 25.8), 0.25, { -- zone in gabz's Davis Departmant
 	name ="analizeblood",
